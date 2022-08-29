a837ec3cde641985d834a11375a229992a9ae8f8
ayoubzitouni <ayoubzitouni315@gmail.com>
2022-08-29 20:23:14 +0100

    final DOM

--- shopping.js
+++ shopping.js
@@ -1,33 +1,33 @@
-var plsbtn=document.querySelectorAll(".pbtn")
+var plsbtn=document.querySelectorAll('.pbtn')
+
 var mnbtn=document.getElementsByClassName("mbtn")
+
 var xbtn=document.getElementsByClassName("delete")
 var total=Number(document.getElementById("price").innerHTML)
 var qty =Number(document.getElementById("qtt").innerHTML)
-console.log(plsbtn)
-for(var btn of plsbtn){
-    btn.addEventListener('click',function(){
+
+for(let btn of plsbtn){
+    btn.addEventListener("click",function(){
         price=Number(btn.parentNode.children[1].children[1].children[0].innerHTML)
-        if(total>=0){
-        total+=price
-        qty+=1
+        qty++;  
+        total=total+price
         document.getElementById("price").innerHTML=total
         document.getElementById("qtt").innerHTML=qty
-        }
     })
 }
-for(var btn of mnbtn){
+
+for(let btn of mnbtn){
     btn.addEventListener('click',function(){
         price=Number(btn.parentNode.children[1].children[1].children[0].innerHTML)
-        if(total>0){
+        if(total>0 ){
             total-=price
             qty-=1
             document.getElementById("price").innerHTML=total
             document.getElementById("qtt").innerHTML=qty
-        }
-        
+        }           
     })
 }
-for(var btn of xbtn){
+for(let btn of xbtn){
     btn.addEventListener('click',function(){
         let action=confirm("delete item")
         if(action){
